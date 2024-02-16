9030a8ec4f56e803b31422a0d42af088a4926f49
Muthiifaith <159955127+Muthiifaith@users.noreply.github.com>
2024-02-16 13:31:25 +0300

    Add files via upload

---
+++ assignment 1 correction/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//ADM N0: ENE212-0072/2021, NAME: MUTHII FAITH NYAWIRA//
+int main()
+{
+    double a=0;
+    double b=0;
+    printf("Enter the first number\n");{
+        scanf("%lf", &a);
+    }
+    printf("Enter the second number\n");{
+        scanf("%lf", &b);
+    }
+    printf("The sum is %lf \n" ,(a+b));
+    printf("The diff is %lf\n" ,(a-b));
+    printf("The sum is %lf and the diff is %lf \n " , (a+b), (a-b));
+    return 0;
+}
